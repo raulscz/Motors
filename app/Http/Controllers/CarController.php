d3e10e20fc39a396ff2f55af8f280b53e84082ba
Raul <100007530.joan23@fje.edu>
2022-01-10 19:33:37 +0100

    first commit

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Car;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Car $car)
+    {
+        //$listaCoches = DB::table('cars')->get();
+        //return view('index', compact('listaCoches'));
+        $car = DB::select('select * from cars'); 
+        return view('cars.index', compact('car'));
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Car $car)
+    {
+        $car = DB::select('select * from cars where id = ?', [$car->id]);
+        return view('cars.show', ['car'=>$car[0]]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Car $car)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Car $car)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Car $car)
+    {
+        //
+    }
+}
---
+++ resources/views/cars/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+    		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    		<title>Mostrar Coches</title>
+	</head>
+	<body>
+    		<div>
+        		<table>
+            		<tr>
+               			<th>ID</th>
+                		<th>MAKE</th>
+                		<th>MODEL</th>
+                		<th>PRODUCED_ON</th>
+                        <th>CREATED_AT</th>
+                        <th>UPDATED_AT</th>
+            		</tr>
+            		@foreach($listaCoches as $coches)
+                	<tr>
+                    		<td>{{$coches->id}}</td>
+                    		<td>{{$coches->make}}</td>
+                    		<td>{{$coches->model}}</td>
+                    		<td>{{$coches->produced_on}}</td>
+                            <td>{{$coches->created_at}}</td>
+                            <td>{{$coches->updated_at}}</td>
+                	</tr>
+            		@endforeach
+        		</table>
+    		</div>
+	</body>
+	</html>
